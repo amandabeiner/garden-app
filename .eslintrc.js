edc63b767602a69b542d180d16a9d334daa117ca
Amanda Beiner <aebeiner@gmail.com>
2020-03-29 18:50:31 -0400

    add typescript and prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,9 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
+  extends: [
+    '@react-native-community',
+    'prettier',
+    'prettier/@typescript-eslint',
+    'prettier/react',
+  ],
 };
